df4db2249c766fca370e07f427d4b8b27a55bedf
Issahaku Abdul Sabur <62177988+ntow@users.noreply.github.com>
2022-07-14 02:57:21 +0000

    modified answers

--- src/assignment.js
+++ src/assignment.js
@@ -9,10 +9,10 @@ const assignment = {};
  *
  * Calculate and return the sum of the numbers in an array.
 If you did Challenge - 1, remove the comment in the line just after this function
-*/
+*
 @param {Array} arrayOfNumbers the array of numbers to sum
 @returns number the sum of the numbers
- 
+ */
 function sumOfNumbers(arrayOfNumbers) {
   let sum = 0;
     for (let num of arrayOfNumbers) {
@@ -27,10 +27,10 @@ assignment.sumOfNumbers = sumOfNumbers;
  * 
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
- */
+ *
 @param {Array} arrayOfNumbers the array containing even or non-even numbers
 @returns number the count of even numbers
- 
+ */
 function countEvenNumbers(arrayOfNumbers) {
   let count = 0;
     for(let i = 0; i < arrayOfNumbers.length; i++) {
